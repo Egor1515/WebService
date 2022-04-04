f1c54c9ba1d2d398a446139d708bd83d1bc97a8a
Egor1515 <96645276+Egor1515@users.noreply.github.com>
2022-04-04 18:21:03 +1000

    Jar file

---
+++ src/test/java/CallBackTest.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class CallBackTest {
+    private WebDriver driver;
+
+
+    @BeforeAll
+    public static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "./driver/windows/chromedriver.exe");
+    }
+
+    @BeforeEach
+
+    public void setUp() {
+
+        driver = new ChromeDriver();
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+
+    public void shouldSendForm() {
+        driver.get("http://localhost:9999/");
+//        driver.findElement().sendKeys("Иван");
+//        driver.findElement().sendKeys("89146865959");
+        List<WebElement> textFields = driver.findElements(By.className("input__control"));
+        textFields.get(0).sendKeys("Иван");
+        textFields.get(1).sendKeys("89146865959");
+        driver.findElement(By.className("checkbox__control")).click();
+        driver.findElement(By.tagName("button")).click();
+        String actualText = driver.findElement(By.className("aller-success")).getText();
+        String expectedText = "Ваша заявка успешно отправлена!";
+        assertEquals(expectedText, actualText);
+
+    }
+
+
+}
