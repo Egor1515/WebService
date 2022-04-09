431f3a1e58f198c9adb0e5120560f21a34217f1d
Egor1515 <96645276+Egor1515@users.noreply.github.com>
2022-04-09 11:43:51 +1000

    WebTests

--- src/test/java/CallBackTest.java
+++ src/test/java/CallBackTest.java
@@ -19,8 +19,7 @@ public class CallBackTest {
     }
 
     @BeforeEach
-        public void setUp() {
-
+    public void setUp() {
         driver = new ChromeDriver();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
@@ -43,14 +42,84 @@ public class CallBackTest {
         driver.get("http://localhost:9999/");
         List<WebElement> textFields = driver.findElements(By.className("input__control"));
         textFields.get(0).sendKeys("Иван");
-        textFields.get(1).sendKeys("89146865959");
-        driver.findElement(By.className("checkbox__control")).click();
-        driver.findElement(By.tagName("button")).click();
-        String actualText = driver.findElement(By.className("aller-success")).getText();
-        String expectedText = "Ваша заявка успешно отправлена!";
+        textFields.get(1).sendKeys("+79146865959");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String actualText = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        String expectedText = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         assertEquals(expectedText, actualText);
 
     }
 
+    @Test
 
+    public void shouldSendForm1() {
+        driver.get("http://localhost:9999/");
+        List<WebElement> textFields = driver.findElements(By.className("input__control"));
+        textFields.get(0).sendKeys("И-И");
+        textFields.get(1).sendKeys("+79146865959");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String actualText = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        String expectedText = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expectedText, actualText);
+    }
+
+    @Test
+
+    public void shouldSendForm2() {
+        driver.get("http://localhost:9999/");
+        List<WebElement> textFields = driver.findElements(By.className("input__control"));
+        textFields.get(0).sendKeys("И");
+        textFields.get(1).sendKeys("+79146865959");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String actualText = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        String expectedText = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expectedText, actualText);
+    }
+
+    @Test
+
+    public void shouldSendForm3() {
+        driver.get("http://localhost:9999/");
+        List<WebElement> textFields = driver.findElements(By.className("input__control"));
+        textFields.get(0).sendKeys("еееееееееееееееееееееееееееееееееееееееееееееееееееееееееееееееееееее");
+        textFields.get(1).sendKeys("+79146865959");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String actualText = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        String expectedText = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expectedText, actualText);
+    }
+
+    @Test
+
+    public void shouldSendForm4() {
+        driver.get("http://localhost:9999/");
+        List<WebElement> textFields = driver.findElements(By.className("input__control"));
+        textFields.get(0).sendKeys("Иван");
+        textFields.get(1).sendKeys("+77777777777");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String actualText = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        String expectedText = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expectedText, actualText);
+    }
+
+    @Test
+
+    public void shouldSendForm5() {
+        driver.get("http://localhost:9999/");
+        List<WebElement> textFields = driver.findElements(By.className("input__control"));
+        textFields.get(0).sendKeys("Иван");
+        textFields.get(1).sendKeys("+79146865959");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String actualText = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        String expectedText = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expectedText, actualText);
+
+
+    }
 }
