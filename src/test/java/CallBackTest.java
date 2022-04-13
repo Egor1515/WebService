f1ad4a0a39792b6cfcbc512d8272c826b6f58ad1
Egor1515 <96645276+Egor1515@users.noreply.github.com>
2022-04-14 08:41:24 +1000

    WebDriver

--- src/test/java/CallBackTest.java
+++ src/test/java/CallBackTest.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -14,26 +15,20 @@ public class CallBackTest {
     private WebDriver driver;
 
     @BeforeAll
-    public static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/windows/chromedriver.exe");
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
-    public void setUp() {
+    void setupTest() {
         driver = new ChromeDriver();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-
     }
 
     @AfterEach
-    public void tearDown() {
-
-        driver.quit();
-        driver = null;
+    void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
     @Test
