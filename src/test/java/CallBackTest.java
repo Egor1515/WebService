31d9d308e69077b18526ac6eeebedf5e0a3c80e6
Egor1515 <96645276+Egor1515@users.noreply.github.com>
2022-04-23 09:16:14 +1000

    appVeyour

--- src/test/java/CallBackTest.java
+++ src/test/java/CallBackTest.java
@@ -15,9 +15,7 @@ public class CallBackTest {
 
     @BeforeAll
     static void setupClass() {
-
         WebDriverManager.chromedriver().setup();
-
     }
 
     @BeforeEach
@@ -26,6 +24,8 @@ public class CallBackTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get("http://localhost:5555/");
+
     }
 
 
@@ -40,7 +40,6 @@ public class CallBackTest {
 
     public void shouldSendForm() {
 
-        driver.get("http://localhost:5555/");
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Константин");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79146865959");
         driver.findElement(By.className("checkbox__box")).click();
@@ -54,7 +53,6 @@ public class CallBackTest {
     @Test
 
     public void shouldSendForm1() {
-        driver.get("http://localhost:5555/");
 
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванова-Петрова Ирина");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79876543210");
@@ -68,7 +66,7 @@ public class CallBackTest {
     @Test
 
     public void shouldSendForm2() {
-        driver.get("http://localhost:5555/");
+
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Андрей О");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79146865959");
         driver.findElement(By.className("checkbox__box")).click();
@@ -81,7 +79,7 @@ public class CallBackTest {
     @Test
 
     public void shouldSendForm3() {
-        driver.get("http://localhost:5555/");
+
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Константинов Константин");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79146865959");
         driver.findElement(By.className("checkbox__box")).click();
@@ -94,7 +92,7 @@ public class CallBackTest {
     @Test
 
     public void shouldSendForm4() {
-        driver.get("http://localhost:5555/");
+
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("О Ян");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79146865959");
         driver.findElement(By.className("checkbox__box")).click();
@@ -107,7 +105,7 @@ public class CallBackTest {
     @Test
 
     public void shouldSendForm5() {
-        driver.get("http://localhost:5555/");
+
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Константинов-Поликарпов Константин");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+77777777777");
         driver.findElement(By.className("checkbox__box")).click();
